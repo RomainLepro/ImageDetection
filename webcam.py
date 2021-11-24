fafd2ad8bd6dd0b6715f58bb02a7ac160a6af777
Romain <tirelalangue98@laposte.net>
2021-11-24 15:07:42 +0100

    first commit image recognition

---
+++ webcam.py
@@ -0,0 +1,24 @@
+import cv2
+
+
+def show_webcam(mirror=False):
+    cam = cv2.VideoCapture(0)
+    ret_val, img = cam.read()
+    cv2.imshow('my webcam', img)
+    while True:
+        ret_val, img = cam.read()
+        if mirror: 
+            img = cv2.flip(img, 1)
+        
+        if cv2.waitKey(1) == 27: 
+            break  # esc to quit
+    cv2.destroyAllWindows()
+
+
+def main():
+    show_webcam(mirror=True)
+
+
+if __name__ == '__main__':
+    print("main")
+    main()

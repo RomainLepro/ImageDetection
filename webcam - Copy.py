6568232131c24dba55a6a77523ecbed2d6dd67b3
Romain <tirelalangue98@laposte.net>
2021-12-29 21:21:39 +0100

    test cam

--- webcam - Copy.py
+++ webcam - Copy.py
@@ -1,8 +1,14 @@
 from picamera import PiCamera
 from time import sleep
 
+# Explicitly open a new file called my_image.jpg
+my_file = open('my_image.jpg', 'wb')
+
 camera = PiCamera()
 camera.start_preview()
-sleep(5)
-camera.capture('/home/pi/Desktop/image.jpg')
-camera.stop_preview()
+sleep(2)
+camera.capture(my_file)
+
+# At this point my_file.flush() has been called, but the file has
+# not yet been closed
+my_file.close()
